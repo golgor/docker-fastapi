c5a863b51be429292de5e9959c7d81d81cb1ade7
Robert Nyström <robert.nystrom@toolsense.io>
2024-07-10 10:48:11 +0200

    added endpoint for hello

--- src/docker_fastapi/main.py
+++ src/docker_fastapi/main.py
@@ -10,6 +10,10 @@ def read_root():
     return {"Hello": "World!"}
 
 
+@app.get("/hello/{name}")
+def hello_name(name: str):
+    return {"message": f"Hello {name}!"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
